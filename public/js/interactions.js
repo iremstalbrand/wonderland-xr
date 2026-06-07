74a480819d060c5e97e47e78fe89b73a0a255aad
simmanomar <omar.ssimman@gmail.com>
2026-06-07 13:00:10 +0200

    fixing mirror interaction and adding clock interaction

--- public/js/interactions.js
+++ public/js/interactions.js
@@ -1,7 +1,22 @@
 const mirror = document.querySelector('#mirror');
 const mirrorText = document.querySelector('#mirrorText');
+const mirrorGlow = document.querySelector('#mirrorGlow');
+const clock = document.querySelector('#clock');
+const clockText = document.querySelector('#clockText');
+
+//mirror interaction
 
 mirror.addEventListener('click', () => {
+   
+ if (mirrorText.getAttribute('visible') === true || mirrorText.getAttribute('visible') === 'true') {
+        return;
+    }
+
+
+   mirrorGlow.setAttribute(
+    'visible',
+    false);
+   
     mirrorText.setAttribute(
         'value',
         'Who am I, really?'
@@ -12,11 +27,59 @@ mirror.addEventListener('click', () => {
         true
     );
 
+   
+
+
     setTimeout(() => {
         mirrorText.setAttribute(
             'value',
             'Who am I, really? \n\n The mirror offers no answer.' //adds response after 3.5 seconds
         );
     }, 3500);
+
+    setTimeout(() => {
+        mirrorText.setAttribute(
+            'visible',
+            false
+        );
+
+        mirrorGlow.setAttribute(
+            'visible',
+             true);
+
+    }, 8000);
+
+});
+
+//clock interaction
+clock.addEventListener('click', () => {
+   
+ if (clockText.getAttribute('visible') === true || clockText.getAttribute('visible') === 'true') {
+        return;
+    }
+
+    clockText.setAttribute(
+        'value',
+        'You are late.'
+    );
+
+    clockText.setAttribute(
+        'visible',
+        true
+    );
+
+    setTimeout(() => {
+        clockText.setAttribute(
+            'value',
+            'You are late. \n\n The clock is watching you.'
+        );
+    }, 3500);
+
+    setTimeout(() => {
+        clockText.setAttribute(
+            'visible',
+            false
+        );
+    }, 8000);
 
 });
