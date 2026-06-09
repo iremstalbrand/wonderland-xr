a36c017ef710d875e8776d259b5c721a91e9c4be
simmanomar <omar.ssimman@gmail.com>
2026-06-09 10:14:11 +0200

    adding background sound and potion interaction

--- public/js/interactions.js
+++ public/js/interactions.js
@@ -3,6 +3,10 @@ const mirrorText = document.querySelector('#mirrorText');
 const mirrorGlow = document.querySelector('#mirrorGlow');
 const clock = document.querySelector('#clock');
 const clockText = document.querySelector('#clockText');
+const clockGlow = document.querySelector('#clockGlow');
+const potion = document.querySelector('#potion');
+const potionText = document.querySelector('#potionText');
+const potionGlow = document.querySelector('#potionGlow');
 
 //mirror interaction
 
@@ -58,6 +62,11 @@ clock.addEventListener('click', () => {
         return;
     }
 
+    clockGlow.setAttribute(
+        'visible',
+        false
+    );
+
     clockText.setAttribute(
         'value',
         'You are late.'
@@ -80,6 +89,71 @@ clock.addEventListener('click', () => {
             'visible',
             false
         );
+
+        clockGlow.setAttribute(
+            'visible',
+            true
+
+        );
+
     }, 8000);
 
 });
+
+// potion interaction
+potion.addEventListener('click', () => {
+
+    if (
+        potionText.getAttribute('visible') === true ||
+        potionText.getAttribute('visible') === 'true'
+    ) {
+        return;
+    }
+
+    potionGlow.setAttribute(
+        'visible',
+        false
+    );
+
+    potionText.setAttribute(
+        'value',
+        "What will happen if I drink?"
+    );
+
+    potionText.setAttribute(
+        'visible',
+        true
+    );
+
+    setTimeout(() => {
+        potionText.setAttribute(
+            'value',
+            "What will happen if I drink?\n\nThe bottle refuses to say."
+        );
+    }, 3500);
+
+    setTimeout(() => {
+
+        potionText.setAttribute(
+            'visible',
+            false
+        );
+
+        potionGlow.setAttribute(
+            'visible',
+            true
+        );
+
+    }, 8000);
+
+});
+
+const ambientAudio = document.querySelector('#ambientAudio');
+
+document.addEventListener(
+  'click',
+  () => {
+    ambientAudio.components.sound.playSound();
+  },
+  { once: true }
+);
