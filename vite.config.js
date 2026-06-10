7afb97df147bec002989d3d9da202edb0e6cccfb
eylulbalcilar <eylulbalcilar@gmail.com>
2026-06-10 22:40:58 +0200

    feat: add flower fields, fix clickable sphere position

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   build: {
     rollupOptions: {
       input: {
